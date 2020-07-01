8d5200d681bfa9acb8f3e3fe6ae019bba66fbc36
Marc Kohaupt <debuglevel@gmail.com>
2020-07-01 16:25:58 +0200

    Do not use cached scripts but clear the cache

--- src/main/kotlin/de/debuglevel/omnitrackergit/script/ScriptService.kt
+++ src/main/kotlin/de/debuglevel/omnitrackergit/script/ScriptService.kt
@@ -1,20 +1,21 @@
 package de.debuglevel.omnitrackergit.script
 
-import de.debuglevel.omnitrackerdatabasebinding.OmnitrackerDatabase
 import de.debuglevel.omnitrackerdatabasebinding.script.Script
+import de.debuglevel.omnitrackerdatabasebinding.script.ScriptService
 import mu.KotlinLogging
 import javax.inject.Singleton
 
 @Singleton
 class ScriptService(
-    private val omnitrackerDatabase: OmnitrackerDatabase
+    private val scriptService: ScriptService
 ) {
     private val logger = KotlinLogging.logger {}
 
     fun getScripts(): Set<Script> {
         logger.debug { "Getting all scripts..." }
 
-        val scripts = omnitrackerDatabase.scripts.values
+        scriptService.clearCache()
+        val scripts = scriptService.getAll().values
 
         logger.debug { "Got ${scripts.size} scripts" }
         return scripts.toSet()
